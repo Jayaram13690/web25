eaeffb5bef875a2dec836c9c9a5cbba617936c67
jayaram233 <20a91a0568@aec.edu.in>
2025-01-06 18:01:42 +0530

    Website git updated

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 let fun = () => {
-    document.write('Wellcome to My webpage')
+    alert('Button Clicked');
 }
+
+const button = document.getElementById("colorButton");
+
+button.addEventListener("click", () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = randomColor;
+});
